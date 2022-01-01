651bf9a4d3182d9fde15ade4869337106d44da19
BenjaminBPJ <pierrejeanbenjamin@hotmail.fr>
2022-01-01 17:51:55 +0100

    nav + debut homepage

---
+++ space-tourisme/src/components/Primary-nav.vue
@@ -0,0 +1,38 @@
+<template>
+    <!-- navigation -->
+    <div>
+        <nav>
+            <ul class="primary-navigation underline-indicators flex">
+                <router-link to="/" class="uppercase text-white letter-spacing-2" tag="li"><span class="primary-nav-span">00</span>Home</router-link>
+                <router-link to="/about" class="uppercase text-white letter-spacing-2" tag="li"><span class="primary-nav-span">01</span>Destination</router-link>
+                <router-link to="/about" class="uppercase text-white letter-spacing-2" tag="li"><span class="primary-nav-span">02</span>Crew</router-link>
+                <router-link to="/tech" class="uppercase text-white letter-spacing-2" tag="li"><span class="primary-nav-span">03</span>Technology</router-link>
+            </ul>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Primary-nav'
+}
+</script>
+
+<style scoped>
+/* interactives navigation and tabs */ 
+.primary-navigation {
+    --gap: 8rem;
+    list-style: none;
+    padding: 0;
+    margin: 0;
+}
+
+.primary-navigation a {
+    text-decoration: none;
+}
+
+.primary-nav-span {
+    font-weight: 700;
+    margin-right: 0.5em;
+}
+</style>
--- space-tourisme/src/views/Home.vue
+++ space-tourisme/src/views/Home.vue
@@ -1,24 +1,29 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <h1 class="text-accent ">So, you want to travel to<span>Space</span></h1>
+      <p>
+        Let’s face it; if you want to go to space, you might as well genuinely
+        go to outer space and not hover kind of on the edge of it. Well sit
+        back, and relax because we’ll give you a truly out of this world
+        experience!
+      </p>
+    </div>
+    <Explore />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Explore from '@/components/Explore.vue'
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    Explore
+  },
+};
 </script>
 
 <style scoped>
-div {
-  background-color: black;
-}
 </style>
