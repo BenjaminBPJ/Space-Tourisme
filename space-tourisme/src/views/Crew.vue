cb65be7510ef2a7d01e185450dc1cb76dee44800
BenjaminBPJ <pierrejeanbenjamin@hotmail.fr>
2022-01-05 18:55:30 +0100

    chgt dynamique page crew

--- space-tourisme/src/views/Crew.vue
+++ space-tourisme/src/views/Crew.vue
@@ -14,28 +14,76 @@
       <span aria-hiden="true">01</span>
       Meet your crew</h1>
 
-      <picture class="crew-image">
+      <picture class="crew-image" v-if="showCommander">
         <source srcset="@/assets/crew/image-douglas-hurley.png" type="image/webp" class="img-crew">
         <img src="@/assets/crew/image-douglas-hurley.png" alt="Douglas Hurley" class="img-crew">
+      </picture>
+      <picture class="crew-image" v-if="showSpecialist">
+        <source srcset="@/assets/crew/image-mark-shuttleworth.png" type="image/webp" class="img-crew">
+        <img src="@/assets/crew/image-mark-shuttleworth.png" alt="Douglas Hurley" class="img-crew">
+      </picture>
+      <picture class="crew-image" v-if="showPilot">
+        <source srcset="@/assets/crew/image-victor-glover.png" type="image/webp" class="img-crew">
+        <img src="@/assets/crew/image-victor-glover.png" alt="Douglas Hurley" class="img-crew">
+      </picture>
+      <picture class="crew-image" v-if="showEngineer">
+        <source srcset="@/assets/crew/image-anousheh-ansari.png" type="image/webp" class="img-crew">
+        <img src="@/assets/crew/image-anousheh-ansari.png" alt="Douglas Hurley" class="img-crew">
       </picture>
 
       <!-- Dots -->
       <div class="dot-indicators flex">
-        <button aria-selected="true"><span class="sr-only">The commander</span></button>
-        <button aria-selected="false"><span class="sr-only">The mission specialist</span></button>
-        <button aria-selected="false"><span class="sr-only">The pilot</span></button>
-        <button aria-selected="false"><span class="sr-only">The crew engineer</span></button>
+        <button aria-selected="true" @click="showCommander=true, showSpecialist=false, showPilot=false, showEngineer=false" @focus="getTabHighlight"><span class="sr-only" >The commander</span></button>
+        <button aria-selected="false" @click="showCommander=false, showSpecialist=true, showPilot=false, showEngineer=false" @focus="getTabHighlight"><span class="sr-only" >The mission specialist</span></button>
+        <button aria-selected="false" @click="showCommander=false, showSpecialist=false, showPilot=true, showEngineer=false" @focus="getTabHighlight"><span class="sr-only" >The pilot</span></button>
+        <button aria-selected="false" @click="showCommander=false, showSpecialist=false, showPilot=false, showEngineer=true" @focus="getTabHighlight"><span class="sr-only" >The crew engineer</span></button>
       </div>
-
       <!-- member description -->
-      <article class="crew-details flow">
+      <article class="crew-details flow" v-if="showCommander">
         <header class="flow">
           <h2 class="crew-grade uppercase ff-serif fs-600">Commander</h2> 
           <p class="crew-name uppercase ff-serif fs-700">Douglas Hurley</p>
         </header>
-        <p class="text-accent"> Douglas Gerald Hurley is an American engineer, former Marine Corps pilot 
+        <p class="text-accent">Douglas Gerald Hurley is an American engineer, former Marine Corps pilot 
           and former NASA astronaut. He launched into space for the third time as 
           commander of Crew Dragon Demo-2.
+        </p>
+      </article>
+
+        <!-- member description -->
+      <article class="crew-details flow" v-if="showSpecialist">
+        <header class="flow">
+          <h2 class="crew-grade uppercase ff-serif fs-600">Mission Specialist</h2> 
+          <p class="crew-name uppercase ff-serif fs-700">Mark Shuttleworth</p>
+        </header>
+        <p class="text-accent">Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind 
+        the Linux-based Ubuntu operating system. Shuttleworth became the first South 
+        African to travel to space as a space tourist.
+        </p>
+      </article>
+
+        <!-- member description -->
+      <article class="crew-details flow" v-if="showPilot">
+        <header class="flow">
+          <h2 class="crew-grade uppercase ff-serif fs-600">Pilot</h2> 
+          <p class="crew-name uppercase ff-serif fs-700">Victor Glover</p>
+        </header>
+        <p class="text-accent">Pilot on the first operational flight of the SpaceX Crew Dragon to the 
+        International Space Station. Glover is a commander in the U.S. Navy where 
+        he pilots an F/A-18.He was a crew member of Expedition 64, and served as a 
+        station systems flight engineer.
+        </p>
+      </article>
+
+        <!-- member description -->
+      <article class="crew-details flow" v-if="showEngineer">
+        <header class="flow">
+          <h2 class="crew-grade uppercase ff-serif fs-600">Flight Engineer</h2> 
+          <p class="crew-name uppercase ff-serif fs-700">Anousheh Ansari</p>
+        </header>
+        <p class="text-accent">Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. 
+        Ansari was the fourth self-funded space tourist, the first self-funded woman to 
+        fly to the ISS, and the first Iranian in space.       
         </p>
       </article>
  
@@ -51,6 +99,26 @@ export default {
   name: "Crew",
   components: {
     Nav,
+  },
+  data () {
+      return {
+        showCommander: true,
+        showSpecialist: false,
+        showPilot: false,
+        showEngineer: false
+      }
+  },
+  methods: {
+    getTabHighlight: function (e) {      
+        const targetTab = e.target;
+        const tabContainer = targetTab.parentNode;
+    
+        tabContainer
+            .querySelector('[aria-selected="true"]')
+            .setAttribute("aria-selected", false);
+
+        targetTab.setAttribute("aria-selected", true);
+    }
   }
 };
 </script>
